3966793d33c378fc0201d1523cd3e4213b6cefce
chaewon kim <cheawon0319@gmail.com>
2025-01-19 00:29:45 -0800

    access the result of the operation represented by the promise

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
+import axios from 'axios'
+
+const promise = axios.get('http://localhost:3001/notes')
+
+promise.then(response => {
+  console.log(response)
+})
 
 const notes = [
   {
