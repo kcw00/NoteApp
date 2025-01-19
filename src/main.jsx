ffaf08978f1061bf50f7aa98e799d9db427ea6bc
chaewon kim <cheawon0319@gmail.com>
2025-01-19 00:35:22 -0800

    format chained method calls

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import axios from 'axios'
 
-const promise = axios.get('http://localhost:3001/notes')
-
-promise.then(response => {
-  console.log(response)
-})
-
+axios
+  .get('http://localhost:3001/notes')
+  .then(response => {
+    const notes = response.data
+    console.log(notes)
+  })
+  
 const notes = [
   {
     id: 1,
