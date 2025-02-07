b6ab77d3db08de9a47c4db1bb4081b7170d736c3
chaewon kim <cheawon0319@gmail.com>
2025-02-07 13:15:44 -0800

    delete try/catch blocks

--- backend/controllers/notes.js
+++ backend/controllers/notes.js
@@ -6,21 +6,17 @@ notesRouter.get('/', async (request, response) => {
   response.json(notes)
 })
 
-notesRouter.get('/:id', async (request, response, next) => {
-  try {
-    const note = await Note.findById(request.params.id)
+notesRouter.get('/:id', async (request, response) => {
+  const note = await Note.findById(request.params.id)
 
-    if (note) {
-      response.json(note)
-    } else {
-      response.status(404).end()
-    }
-  } catch(exception) {
-    next(exception)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
   }
 })
 
-notesRouter.post('/', async (request, response, next) => {
+notesRouter.post('/', async (request, response) => {
   const body = request.body
 
   const note = new Note({
@@ -36,38 +32,28 @@ notesRouter.post('/', async (request, response, next) => {
     return response.status(400).json({ error: validation.message })
   }
 
-  try {
-    const savedNote = await note.save()
-    response.status(201).json(savedNote)
-  } catch (exception) {
-    next(exception)
-  }
+  const savedNote = await note.save()
+  response.status(201).json(savedNote)
 })
 
-notesRouter.delete('/:id', async (request, response, next) => {
-  try {
-    await Note.findByIdAndDelete(request.params.id)
-    response.status(204).end()
-  } catch(exception) {
-    next(exception)
-  }
+notesRouter.delete('/:id', async (request, response) => {
+  await Note.findByIdAndDelete(request.params.id)
+  response.status(204).end()
 })
 
-notesRouter.put('/:id', (request, response, next) => {
+notesRouter.put('/:id', async (request, response) => {
   const { content, important } = request.body
 
-  Note.findByIdAndUpdate(
+  const updatedNote = await Note.findByIdAndUpdate(
     request.params.id,
     { content, important },
     { new: true, runValidators: true, context: 'query' }
   )
-    .then(updatedNote => {
-      if (!updatedNote) {
-        return response.status(404).end()
-      }
-      response.json(updatedNote)
-    })
-    .catch(error => next(error))
+
+  if (!updatedNote) {
+    return response.status(404).end()
+  }
+  response.json(updatedNote)
 })
 
 module.exports = notesRouter
