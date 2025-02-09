bd01f8a5b5c95a643bc2d3411ed76e9187cd20c1
chaewon kim <cheawon0319@gmail.com>
2025-02-09 13:13:58 -0800

    debugging test

--- frontend/src/components/Note.test.jsx
+++ frontend/src/components/Note.test.jsx
@@ -9,6 +9,11 @@ test('renders content', () => {
 
     render(<Note note={note} />)
 
+    // screen.debug() -> print the HTML of components in terminal
+
     const element = screen.getByText('Component testing')
+
+    screen.debug(element)
+
     expect(element).toBeDefined()
 })
