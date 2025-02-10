15b22a0f68ff9128ec4208e4c2d075f23cbd875c
chaewon kim <cheawon0319@gmail.com>
2025-02-10 15:40:06 -0800

    add testingRouter to the backend
    if app runs in test mode

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,6 @@ const mongoose = require('mongoose')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 
-
 mongoose.set('strictQuery', false)
 
 logger.info('connecting to', config.MONGODB_URI)
@@ -31,6 +30,11 @@ app.use(middleware.requestLogger)
 app.use('/api/notes', notesRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
+
+if(process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
