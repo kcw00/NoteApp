4c6d63d43fbe3506352b58edde93bcc55d4760fd
chaewon kim <cheawon0319@gmail.com>
2025-03-30 20:31:01 -0700

    set up mantine in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,44 +4,48 @@ import Notes from './components/Notes'
 import LoginForm from './components/LoginForm'
 import SignupForm from './components/SignupForm'
 import Home from './components/Home'
+import '@mantine/core/styles.css'
+import { MantineProvider } from '@mantine/core'
 
 const App = () => {
   const loggedIn = useSelector(state => Boolean(state.auth.user?.userId))
   const theme = useSelector((state) => state.ui.mode)
 
   return (
-    <BrowserRouter>
-      <div className={theme === "dark" ? "dark-mode" : ""}>
-        <Routes>
-          {/* Auth Routes (only for logged-out users) */}
-          <Route
-            path="/login"
-            element={loggedIn ? <Navigate to="/" /> : <LoginForm />}
-          />
-          <Route
-            path="/signup"
-            element={loggedIn ? <Navigate to="/" /> : <SignupForm />}
-          />
+    <MantineProvider>
+      <BrowserRouter>
+        <div className={theme === "dark" ? "dark-mode" : ""}>
+          <Routes>
+            {/* Auth Routes (only for logged-out users) */}
+            <Route
+              path="/login"
+              element={loggedIn ? <Navigate to="/" /> : <LoginForm />}
+            />
+            <Route
+              path="/signup"
+              element={loggedIn ? <Navigate to="/" /> : <SignupForm />}
+            />
 
 
-          {/* Home Route (redirects to first note if logged in) */}
-          <Route
-            path="/"
-            element={loggedIn ? <Navigate to="/notes" /> : <Home />}
-          />
+            {/* Home Route (redirects to first note if logged in) */}
+            <Route
+              path="/"
+              element={loggedIn ? <Navigate to="/notes" /> : <Home />}
+            />
 
-          {/* Notes Page */}
-          <Route
-            path="/notes"
-            element={loggedIn ? <Notes /> : <Navigate to="/" />}
-          />
-          <Route
-            path="/notes/:id"
-            element={loggedIn ? <Notes /> : <Navigate to="/" />}
-          />
-        </Routes>
-      </div>
-    </BrowserRouter>
+            {/* Notes Page */}
+            <Route
+              path="/notes"
+              element={loggedIn ? <Notes /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/notes/:id"
+              element={loggedIn ? <Notes /> : <Navigate to="/" />}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </MantineProvider>
   )
 }
 
