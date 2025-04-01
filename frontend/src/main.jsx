f1170cc218fd4d2364995db68d50d0e99e44f228
chaewon kim <cheawon0319@gmail.com>
2025-03-31 19:31:05 -0700

    enable react strictmode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,9 @@
 import ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
 import { Provider } from "react-redux"
 import { store } from "./redux/store"
+
+
 
 import App from "./App"
 import './index.css'
@@ -8,7 +11,13 @@ import React from "react"
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <Provider store={store}>
-        <App />
-    </Provider>
+    <React.StrictMode>
+        <BrowserRouter>
+
+            <Provider store={store}>
+                <App />
+            </Provider>
+
+        </BrowserRouter>
+    </React.StrictMode>
 )
