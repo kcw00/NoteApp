020f5b072d3e157a1667a8e9bb81d5bb67f3041b
chaewon kim <cheawon0319@gmail.com>
2025-04-03 12:57:59 -0700

    install apollo client

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,186 @@
+import { useCallback, useEffect, useMemo } from "react"
+import { useDispatch, useSelector } from 'react-redux'
+import { Link, useNavigate } from "react-router-dom"
+import { addNote, setActiveNote, setSharedNotes } from "../redux/notesSlice"
+import { logoutUser } from "../redux/authSlice"
+import { setSidebarWidth, setIsResizing, toggleSidebar } from "../redux/uiSlice"
+import notesService from "../services/notes"
+
+const Sidebar = () => {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const { sidebarWidth, isResizing, isSidebarOpen } = useSelector((state) => state.ui)
+
+    const notes = useSelector(state => state.notes.entities)
+    const notesArray = useMemo(() => Object.values(notes), [notes])
+    const user = useSelector(state => state.auth.user)
+
+    const initialsharedNotes = useSelector(state => state.notes.sharedNotes)
+
+    console.log("initialsharedNotes:", initialsharedNotes)
+
+    console.log('user:', user)
+
+    const [favorites, others, shared] = useMemo(() => {
+        const favs = [], otherNotes = [], sharedNotes = []
+        notesArray.forEach(note => {
+            const isShared = note.collaborators.length > 0
+            if (isShared) {
+                sharedNotes.push(note)
+            }
+
+            if (!isShared && note.important) {
+                favs.push(note)
+            } else if (!isShared && !note.important) {
+                otherNotes.push(note)
+            }
+        })
+        return [favs, otherNotes, sharedNotes]
+    }, [notesArray, user])
+
+    shared.push(...initialsharedNotes)
+
+
+    const resizeSidebar = useCallback((e) => {
+        if (isResizing) {
+            let newWidth = Math.max(300, Math.min(e.clientX, 600)) // Minimum: 300px, Maximum: 600px
+            dispatch(setSidebarWidth(newWidth))
+        }
+    }, [isResizing, dispatch])
+
+    // Function to stop resizing
+    const stopResizing = useCallback(() => {
+        dispatch(setIsResizing(false))
+        document.removeEventListener("mousemove", resizeSidebar)
+        document.removeEventListener("mouseup", stopResizing)
+    }, [dispatch, resizeSidebar])
+
+    // Function to start resizing
+    const startResizing = (e) => {
+        e.preventDefault()
+        dispatch(setIsResizing(true))
+        document.addEventListener("mousemove", resizeSidebar)
+        document.addEventListener("mouseup", stopResizing)
+    }
+
+    useEffect(() => {
+        return () => {
+            document.removeEventListener("mousemove", resizeSidebar)
+            document.removeEventListener("mouseup", stopResizing)
+        }
+    }, [resizeSidebar, stopResizing])
+
+
+    const handleToggleSidebar = () => {
+        dispatch(toggleSidebar())
+    }
+
+    const handleAddNote = () => {
+        const newNote = {
+            id: Math.floor(Math.random() * 1000000),
+            title: "",
+            content: "",
+            important: false,
+            creator: user.userId,
+            collaborators: {},
+        }
+        dispatch(addNote(newNote))
+        dispatch(setActiveNote(newNote.id))
+        setTimeout(() => navigate(`/notes/${newNote.id}`), 50) // Slight delay to allow state update
+    }
+
+    const handleNoteClick = (note) => {
+        dispatch(setActiveNote(note.id))
+        navigate(`/notes/${note.id}`)
+    }
+
+
+    return (
+        <nav className={`sidebar-container ${isSidebarOpen ? "open" : "closed"}`}
+            style={{ width: isSidebarOpen ? `${sidebarWidth}px` : "", transition: isResizing ? "none" : "width 0.2s ease" }}>
+            <div className="sidebar" style={{ width: `${sidebarWidth}px` }}>
+                <div className="sidebar-header">
+                    <p> {user?.username}'s Notes</p>
+                    <button className="fav-button ms-auto" onClick={handleToggleSidebar}>{isSidebarOpen ? "<<" : ""}</button>
+                    <button onClick={handleAddNote}>{"+"}</button>
+                </div>
+                <div className="sidebar-section d-flex flex-column py-3">
+                    <h3>Favorites</h3>
+                    <div className="sidebar-list">
+                        {favorites.map(note => (
+                            <Link
+                                key={note.id}
+                                role="button"
+                                className="favorites"
+                                to={`/notes/${note.id}`}
+                                onClick={() => handleNoteClick(note)}
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-sticky" viewBox="0 0 16 16">
+                                    <path d="M2.5 1A1.5 1.5 0 0 0 1 2.5v11A1.5 1.5 0 0 0 2.5 15h6.086a1.5 1.5 0 0 0 1.06-.44l4.915-4.914A1.5 1.5 0 0 0 15 8.586V2.5A1.5 1.5 0 0 0 13.5 1zM2 2.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 .5.5V8H9.5A1.5 1.5 0 0 0 8 9.5V14H2.5a.5.5 0 0 1-.5-.5zm7 11.293V9.5a.5.5 0 0 1 .5-.5h4.293z" />
+                                </svg>
+                                {" "}{note.title}
+                            </Link>
+                        ))}
+                    </div>
+                    <h3>Others</h3>
+                    <div className="sidebar-list">
+                        {others.map(note => (
+                            <Link
+                                key={note.id}
+                                role="button"
+                                className="others"
+                                to={`/notes/${note.id}`}
+                                onClick={() => handleNoteClick(note)}
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-sticky" viewBox="0 0 16 16">
+                                    <path d="M2.5 1A1.5 1.5 0 0 0 1 2.5v11A1.5 1.5 0 0 0 2.5 15h6.086a1.5 1.5 0 0 0 1.06-.44l4.915-4.914A1.5 1.5 0 0 0 15 8.586V2.5A1.5 1.5 0 0 0 13.5 1zM2 2.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 .5.5V8H9.5A1.5 1.5 0 0 0 8 9.5V14H2.5a.5.5 0 0 1-.5-.5zm7 11.293V9.5a.5.5 0 0 1 .5-.5h4.293z" />
+                                </svg>
+                                {" "}{note.title}
+                            </Link>
+                        ))}
+                    </div>
+                    {shared.length > 0 && (<>
+                        <h3>Shared</h3>
+                        <div className="sidebar-list">
+                            {shared.map(note => (
+                                <Link
+                                    key={note.id}
+                                    role="button"
+                                    className="shared"
+                                    to={`/notes/${note.id}`}
+                                    onClick={() => handleNoteClick(note)}
+                                >
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-sticky" viewBox="0 0 16 16">
+                                        <path d="M2.5 1A1.5 1.5 0 0 0 1 2.5v11A1.5 1.5 0 0 0 2.5 15h6.086a1.5 1.5 0 0 0 1.06-.44l4.915-4.914A1.5 1.5 0 0 0 15 8.586V2.5A1.5 1.5 0 0 0 13.5 1zM2 2.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 .5.5V8H9.5A1.5 1.5 0 0 0 8 9.5V14H2.5a.5.5 0 0 1-.5-.5zm7 11.293V9.5a.5.5 0 0 1 .5-.5h4.293z" />
+                                    </svg>
+                                    {" "}{note.title}
+                                </Link>
+                            ))}
+                        </div>
+                    </>
+                    )}
+                </div>
+                <div className="sidebar-footer text-end p-3 mt-auto">
+                    <Link
+                        className="btn btn-outline-danger"
+                        to="/"
+                        role="button"
+                        onClick={() => dispatch(logoutUser())}>
+                        Sign out
+                    </Link>
+                </div>
+                {/* Resizer Bar (only show when open) */}
+                {isSidebarOpen && (
+                    <div
+                        className="resizer"
+                        onMouseDown={startResizing}
+                    ></div>
+                )}
+            </div>
+        </nav>
+    )
+
+}
+
+export default Sidebar
---
+++ src/services/notes.js
@@ -0,0 +1,102 @@
+import axios from 'axios'
+
+const baseUrl = '/api/notes'
+
+let token = null
+
+// Set the token
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
+
+// Get all notes
+const getAll = async userId => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const request = axios.get(`${baseUrl}/${userId}`, config)
+  return request.then(response => response.data)
+}
+
+// Get a single note
+const getNote = async id => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const request = axios.get(`${baseUrl}/${id}`, config)
+  return request.then(response => response.data)
+}
+
+// Create a new note
+const create = async newObject => {
+  const config = {
+    headers: { Authorization: token },
+  }
+
+  const response = await axios.post(baseUrl, newObject, config)
+  return response.data
+}
+
+// Remove a note
+const remove = (id) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+
+  const request = axios.delete(`${baseUrl}/${id}`, config)
+  return request.then(response => response.data)
+}
+
+// Update a note
+const update = (id, newObject) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+
+  const request = axios.put(`${baseUrl}/${id}`, newObject, config)
+  return request.then(response => response.data)
+}
+
+// Get shared notes
+const getSharedNotes = async (userId) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const request = axios.get(`${baseUrl}/shared`, { userId }, config)
+  return request.then(response => response.data)
+}
+
+// Add collaborator
+const addCollaborator = async (noteId, collaboratorId, userType) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const response = await axios.put(`${baseUrl}/${noteId}/collaborators`, { collaboratorId, userType }, config)
+  return response.data
+}
+
+// Remove collaborator
+const removeCollaborator = async (noteId, collaboratorId) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const response = await axios.delete(`${baseUrl}/${noteId}/collaborators`,
+    {
+      data: { collaboratorId },  // Use 'data' for DELETE request body
+      headers: config.headers,
+    }
+  )
+  return response.data
+}
+
+// Update collaborator role
+const updateCollaboratorRole = async (noteId, collaboratorId, userType) => {
+  const config = {
+    headers: { Authorization: token },
+  }
+  const response = await axios.put(`${baseUrl}//${noteId}/collaborators/${collaboratorId}/role`, { userType }, config)
+  return response.data
+}
+
+
+export default { getAll, getNote, create, update, remove, setToken, addCollaborator, removeCollaborator, updateCollaboratorRole, getSharedNotes }
