e0118b8e4106013d8492a8736b0105f1181a7c4a
chaewon kim <cheawon0319@gmail.com>
2025-04-12 22:26:34 -0700

    add y-websocket in socket.js

--- src/redux/socket.js
+++ src/redux/socket.js
@@ -1,7 +1,18 @@
 import { io } from "socket.io-client"
 
-const socket = io("http://localhost:3001", {
+export const socket = io("http://localhost:3001", {
     transports: ["websocket"]
 })
 
-export default socket
+socket.on("connect", () => {
+    console.log("Socket.io connected")
+})
+
+
+export const ySocket = ({ noteId }) => {
+    const socket = new WebSocket(`ws://localhost:3001/y-websocket/${noteId}`)
+    socket.onopen = () => {
+        console.log("Yjs WebSocket connected")
+    }
+}
+
