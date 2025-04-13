81b8c78fd6674e1bfa2cda423ecad43d779cd30c
chaewon kim <cheawon0319@gmail.com>
2025-04-12 22:28:24 -0700

    add loading until fetch shared notes

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useMemo } from "react"
+import { useEffect, useMemo, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import socket from "../redux/socket"
+import { socket } from "../redux/socket"
 
 import { fetchNotes, addNote, setActiveNote, setActiveUsers, setSharedNotes, fetchSharedNotes, noteDeletedRealtime, setCollaborators } from "../redux/notesSlice"
 import notesService from "../services/notes"
@@ -10,6 +10,8 @@ import Sidebar from "./Sidebar"
 const Notes = () => {
     const dispatch = useDispatch()
 
+    const [loading, setLoading] = useState(true) // Track loading state
+
     const notes = useSelector(state => state.notes.entities)
     const notesArray = useMemo(() => Object.values(notes), [notes])
 
@@ -17,8 +19,9 @@ const Notes = () => {
 
     const activeNoteId = useSelector(state => state.notes.activeNoteId)
     const note = useSelector(state => state.notes.entities[activeNoteId])
-
     const user = useSelector(state => state.auth.user)
+
+
 
     console.log("Component re-rendered") // check when component is re-rendered
     console.log('Notes:', notesArray)
@@ -26,20 +29,21 @@ const Notes = () => {
     console.log('Active Note:', note)
 
 
+
     useEffect(() => {
         console.log("useEffect called")
-    
+
         // Avoid running the effect if the user is not authenticated
         if (!user?.token || !user?.userId) return
-    
+
         console.log('Fetching notes...')
-    
+
         // Only fetch notes if the notesArray is empty
         if (notesArray.length === 0) {
             notesService.setToken(user.token)
-    
+
             console.log('Logged user:', user)
-    
+
             // Fetch shared notes and regular notes concurrently
             const fetchNotesData = async () => {
                 // Fetch regular notes
@@ -56,18 +60,18 @@ const Notes = () => {
                     }))
                     console.log('Adding new empty note')
                 }
-    
+
                 // Fetch shared notes
                 const sharedNotesResult = await dispatch(fetchSharedNotes(user.userId))
                 const sharedNotes = sharedNotesResult.payload
                 console.log('Shared notes:', sharedNotes)
-    
+
                 // Update shared notes only if there are any
                 if (sharedNotes.length > 0) {
                     dispatch(setSharedNotes(sharedNotes))
                     console.log('notes after setting shared notes:', notes)
                 }
-    
+
                 // Set the active note (if none exists)
                 if (!activeNoteId) {
                     const noteToSet = fetchedNotes[fetchedNotes.length - 1] // Use the last fetched note
@@ -76,26 +80,28 @@ const Notes = () => {
                         console.log('Setting active note to:', noteToSet)
                     }
                 }
+                setLoading(false)
             }
-    
+
             fetchNotesData()
-    
+
             // Set up socket listeners only once
             socket.on('activeUsers', (data) => {
                 console.log('socket listen ----- Logged user:', data)
                 dispatch(setActiveUsers(data))
             })
-    
+
+
             socket.on('noteDeleted', (data) => {
                 console.log('socket listen ----- Note deleted:', data)
                 dispatch(noteDeletedRealtime(data))
             })
-    
+
             socket.on('collaboratorAdded', (data) => {
                 console.log('socket listen ----- Collaborator added:', data)
                 dispatch(setCollaborators(data))
             })
-    
+
             socket.on('noteShared', (data) => {
                 console.log('socket listen ----- Note shared:', data)
                 dispatch(setSharedNotes(data))
@@ -104,9 +110,14 @@ const Notes = () => {
             // If active note is not found, default to the last note
             dispatch(setActiveNote(notesArray[notesArray.length - 1]?.id))
         }
-    
+
+
     }, [user?.token, user?.userId, notesArray, activeNoteId])
-    
+
+    if (loading) {
+        return <div>Loading...</div> // Display loading state until data is fetched
+    }
+
 
     return (
         <div id="notes-app">
