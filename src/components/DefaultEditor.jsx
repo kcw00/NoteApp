b638c80611cf7f137d8970c1d86b6797379a26a9
chaewon kim <cheawon0319@gmail.com>
2025-04-21 19:53:15 -0700

    user can type content without error

--- src/components/DefaultEditor.jsx
+++ src/components/DefaultEditor.jsx
@@ -6,12 +6,7 @@ import { HocuspocusProvider, WebSocketStatus } from '@hocuspocus/provider'
 import { updateNote } from '../redux/notesSlice'
 // import { ySocket } from '../redux/socket'
 import MenuBar from './Menubar'
-import { useAtom, atom } from 'jotai'
-import { useLayoutEffect } from 'react'
-import { collabExtensions, mainExtensions } from './Extension'
-import { IndexeddbPersistence } from 'y-indexeddb'
-
-
+import { mainExtensions } from './Extension'
 
 const DefaultEditor = ({ noteId, note }) => {
     const dispatch = useDispatch()
@@ -21,80 +16,91 @@ const DefaultEditor = ({ noteId, note }) => {
 
 
 
+
     const ydoc = useMemo(() => new Y.Doc(), [noteId])
     const [isSynced, setSynced] = useState(false)
 
 
 
-    const provider = new HocuspocusProvider({
-        url: 'ws://localhost:1234',
-        name: noteId,
-        document: ydoc,
-        token: collabToken,
-        connect: false,
-        preserveConnection: false,
-        onAuthenticationFailed: (error) => {
-            console.error('Authentication failed:', error)
-        },
-        onStatus: (status) => {
-            if (status === "connected") {
-                console.log('Connected to provider')
-            }
-        },
-    })
+    const provider = useMemo(() => {
+        if (!collabToken) return null
+
+        return new HocuspocusProvider({
+            url: 'ws://localhost:1234',
+            name: noteId,
+            document: ydoc,
+            token: collabToken,
+            connect: false,
+            preserveConnection: false,
+            onAuthenticationFailed: (error) => {
+                console.error('Authentication failed:', error)
+            },
+            onStatus: (status) => {
+                if (status === "connected") {
+                    console.log('Connected to provider')
+                }
+            },
+        })
+    }, [collabToken, noteId, ydoc])
 
     provider.on('synced', () => {
         console.log('Synced with remote provider')
         setSynced(true)
     })
 
+
     useEffect(() => {
+        if (!provider || !collabToken) return
         provider.connect()
         console.log('Provider connected:', provider)
         return () => {
             provider.destroy()
             console.log('Provider disconnected:', provider)
         }
-    }, [provider, noteId])
+    }, [provider, collabToken])
 
     const unsharedEditor = useEditor({
         extensions: mainExtensions,
         immediatelyRender: true,
         shouldRerenderOnTransaction: true,
         onCreate: ({ editor }) => {
-            if (editor) {
-                editor.commands.setContent(note.content)
+            const xml = ydoc.getXmlFragment('content')
+            console.log('XML Fragment:', xml.toString())
+
+            // Don't apply note.content if Yjs document already has synced data
+            if (editor && xml.length === 0) {
+                editor.commands.setContent(note.content?.default)
                 editor.storage.noteId = noteId
             }
         },
         onUpdate: async ({ editor }) => {
             if (editor.isEmpty) return
             const editorContent = editor.getJSON()
-            debouncedUpdateunSharedContent(editorContent)
+            console.log('Editor content:', editorContent)
         }
     })
 
 
 
 
-    const debouncedUpdateunSharedContent = useCallback((content) => {
-        if (!unsharedEditor) return
+    // const debouncedUpdateunSharedContent = useCallback((content) => {
+    //     if (!unsharedEditor) return
 
-        // If the content is synced, we proceed with the timeout logic.
-        if (isSynced) {
-            // Clear any previous timeouts to prevent redundant API calls.
-            clearTimeout(debouncedUpdateContent.timeout)
+    //     // If the content is synced, we proceed with the timeout logic.
+    //     if (isSynced) {
+    //         // Clear any previous timeouts to prevent redundant API calls.
+    //         clearTimeout(debouncedUpdateunSharedContent.timeout)
 
-            // Set a new timeout for saving content (e.g., 3000 ms = 3 seconds).
-            debouncedUpdateContent.timeout = setTimeout(() => {
-                dispatch(updateNote({
-                    id: noteId,
-                    changes: { content }
-                }))
-                console.log('Shared note content saved:', content)
-            }, 300)  // Timeout set to 3 seconds
-        }
-    }, [unsharedEditor, noteId, isSynced])
+    //         // Set a new timeout for saving content (e.g., 3000 ms = 3 seconds).
+    //         debouncedUpdateunSharedContent.timeout = setTimeout(() => {
+    //             dispatch(updateNote({
+    //                 id: noteId,
+    //                 changes: { content }
+    //             }))
+    //             console.log('Shared note content saved:', content)
+    //         }, 300)  // Timeout set to 3 seconds
+    //     }
+    // }, [unsharedEditor, noteId, isSynced, ydoc])
 
 
     return (
