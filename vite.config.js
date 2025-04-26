3a9322bc9d7c1befb831401ff05608a3d8072171
chaewon kim <cheawon0319@gmail.com>
2025-04-25 20:51:20 -0700

    change backend address

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,32 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [react()],
+    server: {
+      host: true,
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+          secure: false,
+        },
+        '/socket.io': {
+          target: env.VITE_BACKEND_URL,
+          ws: true,
+        },
       },
     },
-    '/socket.io': {
-      target: 'http://localhost:3001',
-      ws: true,
-    }
-  },
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './testSetup.js',
-  },
+    test: {
+      environment: 'jsdom',
+      globals: true,
+      setupFiles: './testSetup.js',
+    },
+  }
 })
+
 
