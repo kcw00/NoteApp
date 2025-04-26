aefad01204eea8517f5c2676becce73d10fca1fd
chaewon kim <cheawon0319@gmail.com>
2025-04-25 20:51:20 -0700

    change backend address

---
+++ src/socket.js
@@ -0,0 +1,9 @@
+import { io } from "socket.io-client"
+
+export const socket = io(import.meta.env.VITE_BACKEND_URL, {
+    transports: ["websocket"]
+})
+
+socket.on("connect", () => {
+    console.log("Socket.io connected")
+})
