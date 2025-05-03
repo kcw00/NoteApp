090af4c599d803b7ef4a82c763129ba053d3a6d3
chaewon kim <cheawon0319@gmail.com>
2025-05-03 13:06:30 -0700

    remove unused lines

--- src/components/Note/Editor/ContentEditor/Extension.jsx
+++ src/components/Note/Editor/ContentEditor/Extension.jsx
@@ -3,8 +3,6 @@ import { Highlight } from '@tiptap/extension-highlight'
 import { TaskList } from '@tiptap/extension-task-list'
 import { TaskItem } from '@tiptap/extension-task-item'
 import { CharacterCount } from '@tiptap/extension-character-count'
-import { Collaboration } from '@tiptap/extension-collaboration'
-import { CollaborationCursor } from '@tiptap/extension-collaboration-cursor'
 import { Placeholder } from '@tiptap/extension-placeholder'
 
 
@@ -16,9 +14,6 @@ export const mainExtensions = [
     CharacterCount.extend().configure({ limit: 10000 }),
     Placeholder.configure({
         placeholder: ({ node }) => {
-            if (node.type.name === 'heading') {
-                return 'Heading'
-            }
             if (node.type.name === 'paragraph') {
                 return 'Write something...'
             }
@@ -26,15 +21,4 @@ export const mainExtensions = [
         includeChildren: true,
         showOnlyWhenEditable: true,
     }),
-]
-
-export const collabExtensions = ({ hocuspocusProvider, currentUser, ydoc }) => [
-    Collaboration.configure({ document: ydoc }),
-    CollaborationCursor.configure({
-        provider: hocuspocusProvider,
-        user: {
-            name: currentUser.name,
-            color: currentUser.color,
-        }
-    })
 ]
