f5c0e6ba917827a483e0222c44d2f53546701c8c
chaewon kim <cheawon0319@gmail.com>
2025-05-21 13:32:28 -0700

    delete auto connect false

--- src/redux/authSlice.js
+++ src/redux/authSlice.js
@@ -9,7 +9,6 @@ export const loginUser = createAsyncThunk("auth/loginUser", async (credentials, 
         const user = await authService.login(credentials)
         window.localStorage.setItem("loggedNoteappUser", JSON.stringify(user))
         console.log('Login succeed:', user)
-        socket.connect()
         socket.emit('loggedUser', user?.userId)
         noteService.setToken(user?.token)
         return user
--- src/socket/socket.js
+++ src/socket/socket.js
@@ -2,7 +2,6 @@ import { io } from "socket.io-client"
 
 export const socket = io(import.meta.env.VITE_BACKEND_URL, {
     transports: ["websocket"],
-    autoConnect: false,
 })
 
 socket.on("connect", () => {
